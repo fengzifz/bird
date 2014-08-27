b3c0a5cc4f053a5977e608e14b719936346eea43
Damon Chen <damon.chen@yeshm.com>
2014-08-28 01:40:53 +0800

    Add language module.

---
+++ languages/zh_CN.js
@@ -0,0 +1,11 @@
+/**
+ * Created by damon on 14-8-28.
+ * 语言包
+ */
+
+var zh_CN = {
+    REGISTER: '注册',
+    LOGIN: '登录'
+}
+
+module.exports = zh_CN;
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,16 @@
  * GET users listing.
  */
 
+/**
+ * 包依赖
+ */
+var zhCN = require('../languages/zh_CN');
+
+/**
+ *
+ * @param req
+ * @param res
+ */
 exports.list = function(req, res) {
     res.send("respond with a resource");
 };
@@ -14,6 +24,6 @@ exports.list = function(req, res) {
  */
 exports.reg = function(req, res) {
     res.render('user/reg', {
-        title: '登录'
+        title: zhCN.REGISTER
     });
 }
