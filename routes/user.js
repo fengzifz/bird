45f04c75fa6529296f454f85ec4faa0848780e57
Damon Chen <damon.chen@yeshm.com>
2014-08-27 19:05:43 +0800

    Add login page.

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,17 @@
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+exports.list = function(req, res) {
+    res.send("respond with a resource");
 };
+
+/**
+ * 登录页面
+ * @param req
+ * @param res
+ */
+exports.login = function(req, res) {
+    res.render('user/login', {
+        title: '登录'
+    });
+}
