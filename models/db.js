c0a94da350302df3d8b22d513b5e104d507b1ff6
Damon Chen <damon.chen@yeshm.com>
2014-09-04 20:47:33 +0800

    Add db.js to get db instance.

---
+++ models/db.js
@@ -0,0 +1,13 @@
+/**
+ * Created by damon on 14-9-4.
+ */
+
+var settings = require('../settings'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
+
+module.exports = new Db(
+    settings.db,
+    new Server(settings.host, Connection.DEFAULT_PORT, {safe: true})
+);
