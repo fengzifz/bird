e0161644e33b001b9173d0928c31dd8784043ea4
Damon Chen <damon.chen@yeshm.com>
2014-09-04 20:35:25 +0800

    Add function to check register information.

--- routes/user.js
+++ routes/user.js
@@ -9,11 +9,10 @@
 var zhCN = require('../languages/zh_CN');
 var express = require('express');
 var router = express.Router();
+var validator = require('validator');
 
 /**
  * 注册页面
- * 密码使用 md5 加密
- * 需要邮箱验证
  * @param req
  * @param res
  */
@@ -23,6 +22,84 @@ router.get('/', function(req, res) {
         title: zhCN.REGISTER
     });
 });
+
+/**
+ * 提交注册表单
+ * md5 加密
+ * 邮箱验证
+ * @param req
+ * @param res
+ */
+router.post('/', function(req, res) {
+    var user = {};
+
+    user.email = (req.body.email).trim();
+    user.username = (req.body.username).trim();
+    user.password = (req.body.password).trim();
+    user.rePassword = (req.body['re-password']).trim();
+
+    // 检查用户信息
+    var err = checkRegisterInfo(user);
+
+    // Display message
+    if (err.length != 0) {
+
+        for (var p in err) {
+            if (err.p) {
+
+            }
+        }
+    }
+
+
+});
+
+
+/**
+ * 检查注册信息
+ * @param user
+ * @returns {{}}
+ */
+function checkRegisterInfo(user) {
+
+    var err = {};
+
+    // 所有信息必须填写
+    for (var p in user) {
+        if (user.p == null || user.p == undefined) {
+            err.all = true;
+            break;
+        }
+    }
+
+    // 验证邮件格式
+    if (!validator.isEmail(user.email)) {
+        err.email = true;
+    }
+
+    // 用户名只能使用字母、数字和下划线
+    var usernamePatt = /([a-zA-Z0-9]|[_])$/;
+    if (!validator.matches(user.username, usernamePatt)) {
+        err.username = true;
+    }
+
+    // 密码长度至少6位
+    if (user.password.length < 6) {
+        err.shortPwd = true;
+    }
+
+    // 密码必须包含数字和字母
+    if (!validator.isAlphanumeric(user.password)) {
+        err.alphanumeric = true;
+    }
+
+    // 密码要一致
+    if (user.password != user.rePassword) {
+        err.notSamePwd = true;
+    }
+
+    return err;
+}
 
 // Express 4.x 需要把 router 暴露
 module.exports = router;
