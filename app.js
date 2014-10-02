4d240bc2e76f0a027ec462ab8e99d64bea408a88
Damon Chen <damon.chen@yeshm.com>
2014-10-03 03:04:34 +0800

    Change the port from 3000 to 80.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var flash = require('connect-flash');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || '3000');
+app.set('port', process.env.PORT || '80');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(favicon());
