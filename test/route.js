849c9289f36f2351c15e4fc617239717a1992aef
Damon Chen <damon.chen@yeshm.com>
2014-10-04 12:46:23 +0800

    Add test cases.

--- test/route.js
+++ test/route.js
@@ -5,16 +5,24 @@
 var request = require('supertest');
 var should = require('should');
 var app = require('../app');
+var path = require('../configs/path_config');
 
 describe('routes/user.js', function() {
 
     // Register page
-    it ('/reg/ should 200', function(done) {
-        request(app).get('/reg/').end(function(err, res) {
+    it ('Register page: /reg/ should 200', function(done) {
+        request(app).get(path.reg).end(function(err, res) {
             res.statusCode.should.equal(200);
             done(err);
         });
     });
 
+    // Homepage
+    it ('Homepage: / should 200', function(done) {
+        request(app).get(path.home).end(function(err, res) {
+            res.statusCode.should.equal(200);
+            done(err);
+        });
+    });
 
 });
