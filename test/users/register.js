199f686b8364a2b8be2b2772b4a2ff045a2a8865
Damon Chen <damon.chen@yeshm.com>
2014-10-09 13:31:35 +0800

    Add unit test for register.

--- test/users/register.js
+++ test/users/register.js
@@ -13,15 +13,55 @@ describe('post routes/user.js', function() {
 
     describe('checkRegisterInfo', function() {
 
-        var user = {
-            email: 'damon.chen',
-            name: 'Damon Chen',
-            password: 111111,
-            rePassword: 111111
-        };
-
         it('Invalid email', function(done) {
+
+            var user = {
+                mail: 'damon.chen',
+                name: 'Damon Chen',
+                password: 111111,
+                rePassword: 111111
+            };
+
             users.checkRegisterInfo(user).message.should.equal(msg.ERR_INVALID_EMAIL);
+            done();
+        });
+
+        it('Not match re-password', function(done) {
+
+            var user = {
+                mail: 'damon.chen@damon.com',
+                name: 'Damon chen',
+                password: 111111,
+                rePassword: 222222
+            };
+
+            users.checkRegisterInfo(user).message.should.equal(msg.ERR_NOT_SAME_PASSWORD);
+            done();
+        });
+
+        it('Empty field', function(done) {
+
+            var user = {
+                mail: 'damon.chen@damon.com',
+                name: '',
+                password: 111111,
+                rePassword: 111111
+            };
+
+            users.checkRegisterInfo(user).message.should.equal(msg.ERR_SHOULD_ENTER_ALL);
+            done();
+        });
+
+        it('Username only can use A-Za-z, 0-9 and _', function(done) {
+
+            var user = {
+                mail: 'damon.chen@damon.com',
+                name: 'damon<',
+                password: 111111,
+                rePassword: 111111
+            };
+
+            users.checkRegisterInfo(user).message.should.equal(msg.ERR_INVALID_USERNAME);
             done();
         });
 
