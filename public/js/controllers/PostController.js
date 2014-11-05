f1989312997ac7ff47127ee80a8065f7522bb1b8
Damon Chen <398846606@qq.com>
2014-11-06 01:34:34 +0800

    Now we can restrict the length of post.

--- public/js/controllers/PostController.js
+++ public/js/controllers/PostController.js
@@ -4,5 +4,21 @@
 
 angular.module('PostController', []).controller('PostController', ['$scope', '$http', function($scope, $http) {
 
+    $scope.maxLength = 50;
+
+    // The object should bind with dom using director `ng-model`
+    $scope.postInput = '';
+
+    $scope.actualLength = $scope.maxLength;
+
+    $scope.countLength = function() {
+        $scope.actualLength = $scope.maxLength - $scope.postInput.length;
+    };
+
+    $scope.doPost = function() {
+
+        console.log($scope.postInput);
+
+    }
 
 }]);
