da68c428ba97a37c8fe1cbf198e9b0451e32b7d6
Damon Chen <398846606@qq.com>
2014-11-29 22:41:02 +0800

    Add some comments.

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,22 @@
 /**
- *
+ * Using sea js as files loader.
  * Created by damon on 14/11/2.
  */
 
 define(function(require, exports, module) {
 
+    // Inject ngSea
     var app = angular.module('app', ['ngSea']);
 
+    // Inject appRoutes
     require('appRoutes')(app);
 
+    // Inject $ngSea when angular is running
     app.run(['$rootScope', '$ngSea', function ($rootScope, $ngSea) {
-
         app = $ngSea(app);
-
     }]);
 
+    // Exports `app` as a module.
     module.exports = app;
 });
 
