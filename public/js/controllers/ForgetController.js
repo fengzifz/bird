9e394068cda28a5ced0cdc209bb9bf2e947153a8
Damon Chen <309946606@qq.com>
2014-12-16 21:31:45 +0800

    Add some comments.

--- public/js/controllers/ForgetController.js
+++ public/js/controllers/ForgetController.js
@@ -10,28 +10,30 @@ define(function(require, exports, module) {
 
             function($scope, $http, $rootScope) {
                 $scope.forget = function() {
-                    // TODO: forget password
+
                     var forgetInfo = {
                         mail: $scope.mail
                     };
 
+                    // Send data to server
                     $http({
                         url: '/user/forget',
                         method: 'post',
                         headers: {'Content-Type': 'application/json'},
                         data: JSON.stringify(forgetInfo)
                     }).success(function(data) {
-                        console.log(data);
 
                         var msg = {};
 
                         msg.error = false;
                         msg.description = data.description;
 
+                        // Some error
                         if (data.error) {
                             msg.error = true;
                         }
 
+                        // Tell MainController to display the message
                         $rootScope.$broadcast('alterMsg', msg);
 
                     }).error(function(err) {
