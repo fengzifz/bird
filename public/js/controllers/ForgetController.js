d1f9df32c08e7b8e41bfb1417ad6a61abf8162ed
Damon Chen <309946606@qq.com>
2014-12-16 21:30:36 +0800

    Now we can get new password by using new API.

--- public/js/controllers/ForgetController.js
+++ public/js/controllers/ForgetController.js
@@ -6,11 +6,37 @@ define(function(require, exports, module) {
 
     module.exports = function(app) {
 
-        app.register.controller('ForgetController', ['$scope', '$http',
+        app.register.controller('ForgetController', ['$scope', '$http', '$rootScope',
 
-            function($scope, $http) {
+            function($scope, $http, $rootScope) {
                 $scope.forget = function() {
                     // TODO: forget password
+                    var forgetInfo = {
+                        mail: $scope.mail
+                    };
+
+                    $http({
+                        url: '/user/forget',
+                        method: 'post',
+                        headers: {'Content-Type': 'application/json'},
+                        data: JSON.stringify(forgetInfo)
+                    }).success(function(data) {
+                        console.log(data);
+
+                        var msg = {};
+
+                        msg.error = false;
+                        msg.description = data.description;
+
+                        if (data.error) {
+                            msg.error = true;
+                        }
+
+                        $rootScope.$broadcast('alterMsg', msg);
+
+                    }).error(function(err) {
+                        console.log(err);
+                    });
                 }
             }
 
